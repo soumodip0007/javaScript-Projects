c61c4db86741238a25d28c198a1b8bc637dde5a1
Soumodip Das <soumodipd567@gmail.com>
2023-01-10 14:49:53 +0530

    dn

--- JavaScriptProjects/Bmi calculator/index.js
+++ JavaScriptProjects/Bmi calculator/index.js
@@ -3,15 +3,19 @@ function calculateBmi() {
     var height = document.bmiForm.height.value
     if (weight > 0 && height > 0) {
         var finalBmi = weight / (height / 100 * height / 100)
+        console.log(finalBmi);
         document.bmiForm.bmi.value = finalBmi
         if (finalBmi < 18.5) {
             document.bmiForm.meaning.value = "That you are too thin."
+            console.log( document.bmiForm.meaning.value)
         }
         if (finalBmi > 18.5 && finalBmi < 25) {
             document.bmiForm.meaning.value = "That you are healthy."
+            console.log( document.bmiForm.meaning.value)
         }
         if (finalBmi > 25) {
             document.bmiForm.meaning.value = "That you have overweight."
+            console.log( document.bmiForm.meaning.value)
         }
     }
     else {
